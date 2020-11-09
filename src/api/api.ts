a59e1c04eb7f9e043c2d8aaf658483388df95134
huangzx <hzhixi9@gmail.com>
2020-11-10 00:01:57 +0800

    add api

--- src/api/api.ts
+++ src/api/api.ts
@@ -81,3 +81,131 @@ export function addAddress(data: any) {
     data,
   });
 }
+
+export function editAddress(data: any) {
+  return request({
+    url: "/address",
+    method: "PUT",
+    data,
+  });
+}
+
+export function deleteAddress(params: { id: number }) {
+  return request({
+    url: "/address",
+    method: "DELETE",
+    params,
+  });
+}
+
+export function getDefaultAddress() {
+  return request({
+    url: "/address/default",
+    method: "GET",
+  });
+}
+
+export function getAddressList(params: {
+  pageNumber: number;
+  pageSize: number;
+}) {
+  return request({
+    url: "/address",
+    method: "GET",
+    params,
+  });
+}
+
+export function getAddressDetail(params: { id: number }) {
+  return request({
+    url: "/address",
+    method: "GET",
+    params,
+  });
+}
+
+// User
+export function getUserInfo() {
+  return request({
+    url: "/user/info",
+    method: "GET",
+  });
+}
+
+export function editUserInfo(data: any) {
+  return request({
+    url: "/user/info",
+    method: "PUT",
+    data,
+  });
+}
+
+export function login(data: any) {
+  return request({
+    url: "/user/login",
+    method: "POST",
+    data,
+  });
+}
+
+export function logout() {
+  return request({
+    url: "/user/logout",
+    method: "POST",
+  });
+}
+
+export function register(data: any) {
+  return request({
+    url: "/user/register",
+    method: "POST",
+    data,
+  });
+}
+
+// Order
+export function createOreder(data: any) {
+  return request({
+    url: "/saveOrder",
+    method: "POST",
+    data,
+  });
+}
+
+export function getOrderList(params: any) {
+  return request({
+    url: "/order",
+    method: "GET",
+    params,
+  });
+}
+
+export function getOrderDetail(params: { id: number }) {
+  return request({
+    url: "/order",
+    method: "GET",
+    params,
+  });
+}
+
+export function cancelOrder(params: { id: number }) {
+  return request({
+    url: `/order/${params.id}/cancel`,
+    method: "PUT",
+  });
+}
+
+export function confirmOrder(params: { id: number }) {
+  return request({
+    url: `/order/${params.id}/finish`,
+    method: "PUT",
+  });
+}
+
+export function payOrder(params: any) {
+  return request({
+    url: "/paySuccess",
+    method: "GET",
+    params,
+  });
+}
